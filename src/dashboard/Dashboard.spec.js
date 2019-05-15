99b0497009a92544e18f501c540b2401b9d1029c
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-15 13:51:56 -0700

    added test for gate button dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import 'react-testing-library/cleanup-after-each';
 import Dashboard from './Dashboard';
 
@@ -20,5 +20,16 @@ describe('<Dashboard/>', () => {
 		expect(gateDispl).toBe('Open');
 		expect(lockBtn).toBe('Lock Gate');
 		expect(gateBtn).toBe('Close Gate');
+	});
+	it('gate button changes gate display status to open or close when gate unlocked', () => {
+		const { getByText, fireEvent } = render(<Dashboard />);
+
+		let gateBtn = getByText('Close Gate').innerHTML;
+		fireEvent(gateBtn);
+		let gateDispl = getByText('Closed').innerHTML;
+		gateBtn = getByText('Open Gate').innerHTML;
+
+		expect(gateDispl).toBe('Closed');
+		expect(gateBtn).toBe('Open Gate');
 	});
 });
