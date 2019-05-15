05f79b1f6de6a08377d13294759d29c5f08e596c
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-15 13:33:02 -0700

    added render test for Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
-// Test away
+import React from 'react';
+
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard/>', () => {
+	it('it renders successfully', () => {
+		render(<Dashboard />);
+	});
+	it('it renders with default state/values', () => {
+		const { getByText } = render(<Dashboard />);
+
+		let lockDispl = getByText('Unlocked').innerHTML;
+		let gateDispl = getByText('Open').innerHTML;
+		let lockBtn = getByText('Lock Gate').innerHTML;
+		let gateBtn = getByText('Close Gate').innerHTML;
+
+		expect(lockDispl).toBe('Unlocked');
+		expect(gateDispl).toBe('Open');
+		expect(lockBtn).toBe('Lock Gate');
+		expect(gateBtn).toBe('Close Gate');
+	});
+});
