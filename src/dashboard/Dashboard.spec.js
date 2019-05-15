2889b96bcb2ea17e387e3ad004c6c8c1e9c86c81
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-15 14:12:57 -0700

    added test for lock and unlocked gate display for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -21,15 +21,35 @@ describe('<Dashboard/>', () => {
 		expect(lockBtn).toBe('Lock Gate');
 		expect(gateBtn).toBe('Close Gate');
 	});
-	it('gate button changes gate display status to open or close when gate unlocked', () => {
-		const { getByText, fireEvent } = render(<Dashboard />);
+	it('changes gate display status to open or close when gate unlocked', () => {
+		const { getByText } = render(<Dashboard />);
 
-		let gateBtn = getByText('Close Gate').innerHTML;
-		fireEvent(gateBtn);
+		let gateBtn = getByText('Close Gate');
+		fireEvent.click(gateBtn);
 		let gateDispl = getByText('Closed').innerHTML;
 		gateBtn = getByText('Open Gate').innerHTML;
 
 		expect(gateDispl).toBe('Closed');
 		expect(gateBtn).toBe('Open Gate');
+	});
+
+	it('changes gate display status to unlocked or locked', () => {
+		const { getByText } = render(<Dashboard />);
+
+		let gateBtn = getByText('Close Gate');
+		fireEvent.click(gateBtn);
+		let lockDispl = getByText('Unlocked').innerHTML;
+		let lockBtn = getByText('Lock Gate').innerHTML;
+
+		expect(lockDispl).toBe('Unlocked');
+		expect(lockBtn).toBe('Lock Gate');
+
+		lockBtn = getByText('Lock Gate');
+		fireEvent.click(lockBtn);
+		lockDispl = getByText('Locked').innerHTML;
+		lockBtn = getByText('Unlock Gate').innerHTML;
+
+		expect(lockDispl).toBe('Locked');
+		expect(lockBtn).toBe('Unlock Gate');
 	});
 });
